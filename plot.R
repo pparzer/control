fde19741c43d4b011636a453f498822e180a400e
Parzer <parzerpeter@w1138852b1.ads.krz.uni-heidelberg.de>
2020-08-12 11:48:51 +0200

    Change y-axis range.

--- plot.R
+++ plot.R
@@ -8,6 +8,7 @@ id = "x"
 d = read.csv(paste0("data/", id, ".csv"))
 
 # plot vertical positions
-plot(d$time, d$mouse_y, type = "l", xlab = "time [s]", ylab = "vertical position")
+plot(d$time, d$mouse_y, type = "l", ylim = c(-0.5, 0.5), 
+     xlab = "time [s]", ylab = "vertical position")
 lines(d$time, d$noise_y, type = "l", lty = "dashed")
 lines(d$time, d$target_y, type = "l", lty = "dotted")
